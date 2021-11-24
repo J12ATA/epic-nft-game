1b78d077997079a9df6857f4958149818c32023c
Jose Figueroa <contact@josefig.com>
2021-11-24 00:32:55 -0800

    updated our characters in our run script

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,11 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy(
-    ['Leo', 'Aang', 'Pikachu'], // Names
+    ['RedMoons', 'MetaCyclops', 'Hypnosis'], // Names
     [
-      'https://i.imgur.com/pKd5Sdk.png', // Images
-      'https://i.imgur.com/xVu4vFL.png',
-      'https://i.imgur.com/WMB6g9u.png',
+      'https://lh3.googleusercontent.com/w3_ePnu29gFaCJe7SsIvei1jK8XtwE7nSN19v5KSMn3dTi55BDfkLgyGm-_LAprXlLgFBC3GxeQB8kBqwjx9e5xYQVOp1D9WznCKyA=w600', // Images
+      'https://lh3.googleusercontent.com/uaQG3ho7915lI9ICAmCLMTZctvd0L6mwhirNTP77CEtqZXaPDRDGXgWSeZoIv3uy6kHy8PJqTpetEyOoNYF1_SKgPlRTvuyoZAF-Gg=w600',
+      'https://lh3.googleusercontent.com/rgfZYZOiyzQ3Da8gV9p4n6qNaXhqcpQhRaV_hvvAU2U34CxOAxi9w35VPtI1lVOXa0RbT-icvM_r7-7v10sDG_GwzlW9V4w2PRUdl38=w600',
     ],
     [100, 200, 300], // HP values
     [100, 50, 25] // Attack damage values
