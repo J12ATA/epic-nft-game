0a093187daae07be8d5d5c6ee8f25f38f5c16863
Jose Figueroa <contact@josefig.com>
2021-11-24 01:48:11 -0800

    update our attackBoss function to check HP

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -156,11 +156,24 @@ contract MyEpicGame is ERC721 {
     return output;
   }
 
-  function attackBoss() public view {
+  function attackBoss() public {
     // Get the state of the player's NFT.
     uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
     CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
+
     console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
     console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+
+    // Make sure the player has more than 0 HP.
+    require (
+      player.hp > 0,
+      "Error: character must have HP to attack boss."
+    );
+
+    // Make sure the boss has more than 0 HP.
+    require (
+      bigBoss.hp > 0,
+      "Error: boss must have HP to attack boss."
+    );
   }
 }
