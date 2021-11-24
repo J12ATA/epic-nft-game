8cbc7a726b93cca9dedd9f4900663fe99f1a2769
Jose Figueroa <contact@josefig.com>
2021-11-24 01:50:06 -0800

    update our run script to test attackBoss function

--- scripts/run.js
+++ scripts/run.js
@@ -22,6 +22,9 @@ const main = async () => {
   let txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
 
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
   // Get the value of the NFT's URI.
   let returnedTokenUri = await gameContract.tokenURI(1);
   console.log('Token URI:', returnedTokenUri);
